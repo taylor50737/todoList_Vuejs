a91e5e8189c557dcd33f20d6dd5b473a8464cbc9
Taylor Lo <97694902+taylor50737@users.noreply.github.com>
2023-01-14 18:10:34 -0500

    Initial Setup

---
+++ src/App.vue
@@ -0,0 +1,121 @@
+<script setup>
+import { ref, onMounted, computed, watch } from 'vue';
+
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_category = ref(null)
+
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+  return b.createdAt - a.createAt
+}))
+
+const addTodo = () => {
+  if (input_content.value.trim() === '' || input_category.value === null) {
+    return
+  }
+
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime()
+  })
+
+  input_content.value = ''
+  input_category.value = null
+}
+
+const removeTodo = todo => {
+  todos.value = todos.value.filter(t => t !== todo)
+}
+
+watch(todos, newVal => {
+  localStorage.setItem('todos', JSON.stringify(newVal))
+}, { deep: true })
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || ''
+  todos.value = JSON.parse(localStorage.getItem('todos')) || []
+})
+</script>
+
+<template>
+
+  <main class="app">
+
+    <section class="greeting">
+      <h2 class="title">
+        今日有咩搞呀？
+        <input
+          type="text"
+          placeholder="你的名字"
+          v-model="name"
+        />
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>創建待辦事項</h3>
+
+      <form @submit.prevent="addTodo">
+        <h4>有咩要做呀？</h4>
+        <input
+            type="text"
+            placeholder="e.g. Coding"
+            v-model="input_content"
+        />
+        <h4>揀返個分類先啦</h4>
+
+        <div class="options">
+          <label>
+            <input
+                type="radio"
+                name="category"
+                value="business"
+                v-model="input_category"
+            />
+            <span class="bubble business"></span>
+            <div>工作</div>
+          </label>
+          <label>
+            <input
+                type="radio"
+                name="category"
+                value="personal"
+                v-model="input_category"
+            />
+            <span class="bubble personal"></span>
+            <div>私人</div>
+          </label>
+
+        </div>
+
+        <input type="submit" value="創建待辦事項" />
+      </form>
+    </section>
+
+    <section class="todo-list">
+      <h3>待辦事項</h3>
+      <div class="list">
+        <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+          <label>
+            <input type="checkbox" v-model="todo.done" />
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+          <div class="todo-content">
+            <input type="text" v-model="todo.content" />
+          </div>
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">搞掂！</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
